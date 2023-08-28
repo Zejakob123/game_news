0f69ccc7a63132ea14a78d0f3b555df6216995e5
Zejakob123 <jzleong@outlook.com>
2023-08-28 13:07:50 +0800

    game_news project commit

---
+++ game_news/lib/repository/game_repository.dart
@@ -0,0 +1,16 @@
+import 'package:game_news/repository/models/model_barrel.dart';
+import 'package:game_news/repository/service/game_service.dart';
+
+class GameRepository {
+  const GameRepository({
+    required this.service,
+  });
+  final GameService service;
+
+  Future<Game> getGames() async => service.getGames();
+
+  Future<List<Genre>> getGenres() async => service.getGenres();
+
+  Future<List<Result>> getGamesByCategory(int genreId) async =>
+      service.getGamesByCategory(genreId);
+}
